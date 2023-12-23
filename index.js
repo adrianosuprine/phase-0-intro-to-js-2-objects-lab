99ad29e95adf9b4023c05135cf4e3d3c93c8041d
adrianosuprine <adrianosuprine@gmail.com>
2023-12-23 23:29:47 +0300

    Manipulate js objects

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+let employee = {
+    name:'Adriano',
+    streetAddress: 'Chiromo Lane',
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+  let employeeTwo = {... employee};
+  employeeTwo = {
+    name : "Sam", streetAddress : "11 Broadway"}
+  return employeeTwo;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+  employee.name = "Sam";
+  employee.streetAddress= "12 Broadway";
+  return employee;
+
+}
+function deleteFromEmployeeByKey(employee,key){
+  let employee_2 = {... employee};
+  delete employee_2.name;
+  return employee_2;
+
+}
+function destructivelyDeleteFromEmployeeByKey(){
+  delete employee.name;
+  return employee;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
